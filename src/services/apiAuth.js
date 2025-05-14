f481e1ee2da5fcc8baf3fbe61d5acc60e337f644
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-05-14 13:53:46 +0400

    update user data and password

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,12 +1,13 @@
-import supabase from "./supabase";
+import { da } from "date-fns/locale";
+import supabase, { supabaseUrl } from "./supabase";
 
-export async function signup({ fullname, email, password }) {
+export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
     options: {
       data: {
-        fullname,
+        fullName,
         avatar: "",
       },
     },
@@ -34,7 +35,6 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
   if (error) throw new Error(error.message);
 
   return data?.user;
@@ -43,4 +43,41 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. update password or fullname
+
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser({
+    password,
+    data: { fullName },
+  });
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. upload avatar image
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. update avatar in the user
+
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars//${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
 }
